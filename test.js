eecf1aa608b79d669403ed3cead67605213473f1
Monica <monicajromero5683@hotmail.com>
2019-02-12 22:44:28 -0500

    Changed code in test.js to fit lesson.

--- test.js
+++ test.js
@@ -1,18 +1,11 @@
-var youLikeMeat = true;
-var myNum = 10;
+var myAge = 21;
 
-
-if (youLikeMeat){
-
-      document.write("You like meat. This expression is true.");
-}
-
-document.write("<br>");
-document.write("<br>");
-
-if (myNum > 9) {
-  document.write("Number is equal to 10.");
-
-}else{
-  document.write("Number is 9. Number is not equal to 10.");
+if (myAge > 20) {
+  document.write("You are over 20!");
+} else if (myAge > 19) {
+  document.write("You are over 19!");
+} else if (my > 17) {
+  document.write("You are over 17!");
+} else { 
+  document.write("You are not over 10 years old.");
 }
